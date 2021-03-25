7dc8ae4cd6286efdfe16a1b29977ee18079960b5
Tomás Badenes <tomasbadenes@gmail.com>
2021-03-25 10:30:09 -0300

    The board was initialized mirrored. Fixed

--- src/Board.cpp
+++ src/Board.cpp
@@ -10,10 +10,10 @@ void Board::Init(int mCellSize)
 	for (int i=0; i<7; i++) {
 		mBoardCells.push_back({});
 		for (int j=0; j<3+!(i%2); j++) {
-			sf::Vector2f pos = sf::Vector2f(j*mCellSize*2+(i%2)*mCellSize, i*mCellSize);
+			sf::Vector2f pos = sf::Vector2f(j*mCellSize*2+(i%2)*mCellSize, mCellSize*(6-i));
 			Cell* cell = new Cell(mCellSize, pos, codes::CellID((j*2+i%2), i));
 			mBoardCells[i].push_back(cell);
-			if (i!=3) cell->PushPiece(new Piece(i<3, cell));
+			if (i!=3) cell->PushPiece(new Piece(i>3, cell));
 		}
 	}
 }
@@ -22,7 +22,7 @@ Cell* Board::GetCellByID(codes::CellID ID)
 {
 	int x = (ID.x-(ID.y%2))/2;
 	int y = ID.y;
-	return mBoardCells[x][y];
+	return mBoardCells[y][x];
 }
 
 std::vector<std::vector<Cell*>> Board::GetCells()
