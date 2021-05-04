a0ab281665c5952260bb9fde4beaa81271569a6d
Tomás Badenes <tomasbadenes@gmail.com>
2021-05-03 21:08:33 -0300

    Old completed TODO removed

--- src/Piece.cpp
+++ src/Piece.cpp
@@ -1,8 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include "Piece.h"
 
-// TODO each piece should have its own offset based on its position on the tower.
-// In that case, don't change this
 
 Piece::Piece(bool color, Cell* cell) : mColor(color)
 {
